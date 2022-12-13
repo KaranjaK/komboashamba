14dca9befe869ddd28e7dc893bfe1b940d7aec89
Kelvin <karanjakelvin77@gmail.com>
2022-12-13 11:12:11 +0300

    It will adjust JWT settings on the database

--- KomboaShamba/settings.py
+++ KomboaShamba/settings.py
@@ -50,7 +50,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
