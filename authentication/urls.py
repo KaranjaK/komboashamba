df4ff5869768b807eb1c461a607fc4d84f149708
Kelvin <karanjakelvin77@gmail.com>
2022-12-13 05:52:39 +0300

    It will create router for signup

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router .register('', views.USerViewSet)
+
 urlpatterns = [
-    # # path('', views.home, name='home'),
-    # path('signup', views.signup, name='singup'),
-    # path('signin', views.signin, name='singin'),
-    # path('signout', views.signout, name='singout'),
+    path('api-auth/', include('rest_framework.urls'))
 ]
+urlpatterns += router.urls
